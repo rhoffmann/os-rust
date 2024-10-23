dd79b80a3dd0b46b5da9f21d8d3d7e6dc60483e2
Richard Hoffmann <rhoffmann@fastmail.com>
2024-10-23 21:32:49 +0200

    add customized test runner

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 #![no_std] // dont link rust std lib
 #![no_main] // disable rust-level entry point
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 mod vga_buffer;
 use core::panic::PanicInfo;
@@ -10,10 +13,28 @@ fn panic(info: &PanicInfo) -> ! {
     loop {}
 }
 
+#[cfg(test)]
+pub fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+}
+
 #[no_mangle] // keep name of the function as is
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
 
+    #[cfg(test)]
+    test_main();
+
     // panic!("Some panic message!");
     loop {}
+}
+
+#[test_case]
+fn trivial_assert() {
+    print!("trivial assertion...");
+    assert_eq!(1, 1);
+    println!("[ok]");
 }
