2e99bb7b81b89279e7a32317757abf50d4d6598d
Richard Hoffmann <rhoffmann@fastmail.com>
2024-10-23 18:01:47 +0200

    simple vga buffer output and toolchain changes

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
-#![no_std]
-#![no_main]
+#![no_std] // dont link rust std lib
+#![no_main] // disable rust-level entry point
+
+static HELLO: &[u8] = b"Hello World!";
 
 use core::panic::PanicInfo;
 
@@ -8,7 +10,15 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-#[no_mangle]
+#[no_mangle] // keep name of the function as is
 pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
     loop {}
 }
