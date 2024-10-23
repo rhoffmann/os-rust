8df6a5a1176b86efc634fdecc7c5a6288fcd5ada
Richard Hoffmann <rhoffmann@fastmail.com>
2024-10-23 20:12:42 +0200

    add abstractions on vga buffer

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
 #![no_std] // dont link rust std lib
 #![no_main] // disable rust-level entry point
 
-static HELLO: &[u8] = b"Hello World!";
-
+mod vga_buffer;
 use core::panic::PanicInfo;
 
 #[panic_handler]
@@ -12,13 +11,7 @@ fn panic(_info: &PanicInfo) -> ! {
 
 #[no_mangle] // keep name of the function as is
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    vga_buffer::print_something();
 
     loop {}
 }
