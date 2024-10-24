312097341d71a9acd72d996391a8a214377655ea
Richard Hoffmann <rhoffmann@fastmail.com>
2024-10-24 09:00:15 +0200

    extract lib

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,21 @@
 #![no_std] // dont link rust std lib
 #![no_main] // disable rust-level entry point
-#![feature(custom_test_frameworks)] // adapt test framework bc default depends on std lib which is not linked
-#![test_runner(crate::test_runner)]
+#![feature(custom_test_frameworks)]
+#![test_runner(os_rust::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
-mod serial;
-mod vga_buffer;
 use core::panic::PanicInfo;
+use os_rust::println;
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-#[repr(u32)]
-pub enum QemuExitCode {
-    Success = 0x10,
-    Failed = 0x11,
-}
+#[no_mangle] // keep name of the function as is
+pub extern "C" fn _start() -> ! {
+    println!("Hello World{}", "!");
 
-pub fn exit_qemu(exit_code: QemuExitCode) {
-    use x86_64::instructions::port::Port;
+    #[cfg(test)]
+    test_main();
 
-    unsafe {
-        let mut port = Port::new(0xf4);
-        port.write(exit_code as u32);
-    }
-}
-
-pub trait Testable {
-    fn run(&self) -> ();
-}
-
-impl<T> Testable for T
-where
-    T: Fn(),
-{
-    fn run(&self) {
-        serial_print!("{}...\t", core::any::type_name::<T>());
-        self();
-        serial_println!("[ok]");
-    }
+    // panic!("Some panic message!");
+    loop {}
 }
 
 #[cfg(not(test))]
@@ -49,30 +28,7 @@ fn panic(info: &PanicInfo) -> ! {
 #[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
-    serial_println!("[failed]\n");
-    serial_println!("Error: {}\n", info);
-    exit_qemu(QemuExitCode::Failed);
-    loop {}
-}
-
-#[cfg(test)]
-pub fn test_runner(tests: &[&dyn Testable]) {
-    serial_println!("Running {} tests", tests.len());
-    for test in tests {
-        test.run();
-    }
-    exit_qemu(QemuExitCode::Success);
-}
-
-#[no_mangle] // keep name of the function as is
-pub extern "C" fn _start() -> ! {
-    println!("Hello World{}", "!");
-
-    #[cfg(test)]
-    test_main();
-
-    // panic!("Some panic message!");
-    loop {}
+    os_rust::test_panic_handler(info);
 }
 
 #[test_case]
